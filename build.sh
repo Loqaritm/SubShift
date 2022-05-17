373198eab80822d2972966cff601f745602e2f41
Marcel Śliwiński <marcel.sliwinski@gmail.com>
2022-05-17 17:08:25 +0200

    Modified build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-g++ -std=c++11 -I include source/main.cpp -o subshift
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+g++ -std=c++11 -I $SCRIPT_DIR/include $SCRIPT_DIR/source/main.cpp -o "$PWD"/subshift
